191a596f4a3e2aa83c4ed4119778c28c0071081f
elhawari1 <elhawari1234@gmail.com>
2021-10-25 18:51:56 +0700

    arsip surat

---
+++ application/views/about.php
@@ -0,0 +1,17 @@
+<div style="margin-left: 43px;">
+	<h2 class="mt-4">About</h2>
+</div>
+
+<div class="row" style="margin-left: 35px;margin-top:20px; margin-right: -30px;">
+	<div style="flex: 0 0 auto; width: 45%;">
+		<img src="<?= base_url('uploads/pp.png') ?>" style="border:5px solid black;border-radius:30px;" alt="PP">
+	</div>
+	<div class="col-md-6 mb-5">
+		<p>
+		<h4> Aplikasi ini dibuat oleh : </h4>
+		<h6> Nama &nbsp; &nbsp; &nbsp;: Abdelas Yoshia Elhawari </h6>
+		<h6> Nim &nbsp; &nbsp; &nbsp; &nbsp; : 1831710053 </h6>
+		<h6> Tanggal &nbsp; : 24 Oktober 2021 </h6>
+		</p>
+	</div>
+</div>
